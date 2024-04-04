df3c89d8331b3ff401e4d636a8e1623aaafbdba3
Paul Lorber <paul.lorber@protonmail.com>
2024-04-04 18:00:40 +0200

    Add getFavorite function and console.log statements in addFavorite and remFavorite functions

--- controllers/Favorite.js
+++ controllers/Favorite.js
@@ -1,5 +1,28 @@
 const { User } = require("../schemas/User");
 
+const getFavorite = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const user = await User.findById(userId).populate('favorites');
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+
+        res.status(200).json({
+            message: "User found",
+            data: user.favorites,
+        });
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || "An unknown error occurred",
+        });
+    }
+};
+
+
 const addFavorite = async (req, res) => {
     try {
         const userId = req.user._id;
@@ -13,6 +36,7 @@ const addFavorite = async (req, res) => {
         }
         
         const result = await user.addFavorite(addedFavorite);
+        console.log(result);
         return res.status(200).json(result);
     } catch (error) {
         res.status(500).json({
@@ -35,6 +59,7 @@ const remFavorite = async (req, res) => {
         }
 
         const result = await user.remFavorite(removedFavorite);
+        console.log(result);
         return res.status(200).json(result);
     } catch (error) {
         res.status(500).json({
