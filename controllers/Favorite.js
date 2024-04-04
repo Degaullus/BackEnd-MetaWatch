f86969832b3cfffde1f83e36ace537d788b71e0b
Paul Lorber <paul.lorber@protonmail.com>
2024-04-04 16:05:08 +0200

    Add addFavorite function to Favorite controller

--- controllers/Favorite.js
+++ controllers/Favorite.js
@@ -1,2 +1,22 @@
 const { User } = require("../schemas/User");
 
+const addFavorite = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const { favoriteId } = req.body;
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+        
+        const result = await user.addFavorite(favoriteId);
+        return res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || "An unknown error occurred",
+        });
+    }
+};
