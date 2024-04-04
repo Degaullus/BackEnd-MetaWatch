6be73b128563685b537ec0d893cd07a8c489fdff
Paul Lorber <paul.lorber@protonmail.com>
2024-04-04 16:06:13 +0200

    Add remFavorite function to Favorite controller

--- controllers/Favorite.js
+++ controllers/Favorite.js
@@ -20,3 +20,25 @@ const addFavorite = async (req, res) => {
         });
     }
 };
+
+
+const remFavorite = async (req, res) => {
+    try {
+        const userId = req.user._id;
+        const { favoriteId } = req.body;
+
+        const user = await User.findById(userId);
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found",
+            });
+        }
+
+        const result = await user.remFavorite(favoriteId);
+        return res.status(200).json(result);
+    } catch (error) {
+        res.status(500).json({
+            message: error.message || "An unknown error occurred",
+        });
+    }
+};
