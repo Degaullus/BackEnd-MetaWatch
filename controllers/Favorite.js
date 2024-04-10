c63d66f56b0dd0d6bf9b1f71deeaee225e880c72
Paul Lorber <paul.lorber@protonmail.com>
2024-04-10 14:14:58 +0200

    Refactor Favorite.js controller to remove getFavorites function

--- controllers/Favorite.js
+++ controllers/Favorite.js
@@ -1,28 +1,5 @@
 const { User } = require("../schemas/User");
 
-const getFavorites = async (req, res) => {
-    try {
-        const userId = req.user._id;
-        const user = await User.findById(userId).populate('favorites');
-
-        if (!user) {
-            return res.status(404).json({
-                message: "User not found",
-            });
-        }
-
-        res.status(200).json({
-            message: "User found",
-            data: user.favorites,
-        });
-    } catch (error) {
-        res.status(500).json({
-            message: error.message || "An unknown error occurred",
-        });
-    }
-};
-
-
 const addFavorite = async (req, res) => {
     try {
         const userId = req.user._id;
@@ -68,4 +45,4 @@ const remFavorite = async (req, res) => {
     }
 };
 
-module.exports = { getFavorites, addFavorite, remFavorite };
+module.exports = { addFavorite, remFavorite };
