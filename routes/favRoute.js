c0340f66d9fbcfd7fca243ea453cecc4a4a9c3ec
Paul Lorber <paul.lorber@protonmail.com>
2024-04-10 14:15:40 +0200

    Remove getFavorites function from favRoute.js

--- routes/favRoute.js
+++ routes/favRoute.js
@@ -1,10 +1,9 @@
 const express = require('express');
 
-const { getFavorites, addFavorite, remFavorite } = require("../controllers/Favorite");
+const { addFavorite, remFavorite } = require("../controllers/Favorite");
 
 const app = express.Router();
 
-app.get("/get", getFavorites);
 app.post("/add", addFavorite);
 app.post("/remove", remFavorite);
 
