18142eb3bca5c7d17cae81d7974dc8cab27a9522
Paul Lorber <paul.lorber@protonmail.com>
2024-04-11 14:25:06 +0200

    same as before, only correct

--- schemas/User.js
+++ schemas/User.js
@@ -37,6 +37,33 @@ const favoriteSchema = new mongoose.Schema({
 });
 
 // to create a new method for the signup
+userSchema.statics.signup = async function(username, email, password) {
+    const exists = await this.findOne({ email }); // Fixed method call
+    
+    if (exists) {
+        throw new Error('Email already in use');
+    }
+    
+    if (!email || !password) { // Corrected syntax
+        throw new Error("All fields must be filled");
+    }
+    if (!validator.isEmail(email)) { // Corrected syntax
+        throw new Error("Invalid Email");
+    }
+
+    if (password.length < 6) { // Example: only check for a minimum length of 6 characters
+        throw new Error("Password must be at least 6 characters long");
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    
+    const hash = await bcrypt.hash(password, salt);
+    
+    const user = await this.create({username,  email, password: hash });
+    
+    return user;
+}
+
 userSchema.statics.login = async function(identifier, password) {
     if (!identifier || !password) {
         throw new Error("All fields must be filled");
@@ -60,38 +87,6 @@ userSchema.statics.login = async function(identifier, password) {
     return user;
 }
 
-
-userSchema.statics.login = async function(email, password) {
-    if(!email || !password) {
-        throw new Error("All fields must be filled");
-    }
-    
-    const user = await this.findOne({ email });
-    
-    if(!user) {
-        throw new Error("Email is incorrect or it doesn't exist");
-    }
-    
-    const match = await bcrypt.compare(password, user.password);
-    
-    if(!match) {
-        throw new Error("Incorrect password");
-    }
-    
-    return user;
-}
-
-userSchema.methods.addFavorite = async function(addedFavorite) {
-    const isAlreadyFavorite = this.favorites.some(fav => fav.equals(addedFavorite));
-    if (!isAlreadyFavorite) {
-        this.favorites.push(addedFavorite);
-        await this.save();
-        return { message: "Added to favorites successfully." };
-    } else {
-        return { message: "Already in favorites, try adding another one." };
-    
-    }
-};
 
 userSchema.methods.remFavorite = async function(removedFavorite) {
     const favoritesBefore = this.favorites.length;
